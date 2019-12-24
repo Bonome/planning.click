da7f163dda77dc637fc25130022445086f10cb86
Bonome <bonome@tak.lu>
2019-12-24 17:24:52 +0100

    add sapper + 1 dummy not working route

--- rollup.config.js
+++ rollup.config.js
@@ -1,94 +1,142 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import pkg from './package.json';
 
-const production = !process.env.ROLLUP_WATCH;
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-//		dir: 'public/build/'
-		file: 'public/build/bundle.js'
-	},
-        
-        moduleContext: (id) => {
-            const thisAsWindowForModules = [
-                'node_modules/intl-messageformat/lib/core.js',
-                'node_modules/intl-messageformat/lib/compiler.js',
-                'node_modules/intl-messageformat/lib/formatters.js',
-                'node_modules/intl-format-cache/lib/index.js',
-                'node_modules/intl-messageformat-parser/lib/normalize.js',
-                'node_modules/intl-messageformat-parser/lib/parser.js',
-                'node_modules/intl-messageformat-parser/lib/skeleton.js,',
-                'node_modules/intl-messageformat-parser/lib/skeleton.js',
-            ];
-
-            if (thisAsWindowForModules.some(id_ => id.trimRight().endsWith(id_))) {
-                return 'window';
-            }
-        },
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
-		commonjs(),
-                json({
+	client: {
+		input: config.client.input(),
+		output: config.client.output(),
+//        moduleContext: (id) => {
+//            const thisAsWindowForModules = [
+//                'node_modules/intl-messageformat/lib/core.js',
+//                'node_modules/intl-messageformat/lib/compiler.js',
+//                'node_modules/intl-messageformat/lib/formatters.js',
+//                'node_modules/intl-format-cache/lib/index.js',
+//                'node_modules/intl-messageformat-parser/lib/normalize.js',
+//                'node_modules/intl-messageformat-parser/lib/parser.js',
+//                'node_modules/intl-messageformat-parser/lib/skeleton.js,',
+//                'node_modules/intl-messageformat-parser/lib/skeleton.js',
+//            ];
+//
+//            if (thisAsWindowForModules.some(id_ => id.trimRight().endsWith(id_))) {
+//                return 'window';
+//            }
+//        },
+		plugins: [
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+				dev,
+				hydratable: true,
+				emitCss: true
+			}),
+			resolve({
+				browser: true,
+				dedupe
+			}),
+			commonjs(),
+            
+            json({
                     namedExports: false,
-                    compact:production
-                }),
+                    compact:!dev
+            }),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+			legacy && babel({
+				extensions: ['.js', '.mjs', '.html', '.svelte'],
+				runtimeHelpers: true,
+				exclude: ['node_modules/@babel/**'],
+				presets: [
+					['@babel/preset-env', {
+						targets: '> 0.25%, not dead'
+					}]
+				],
+				plugins: [
+					'@babel/plugin-syntax-dynamic-import',
+					['@babel/plugin-transform-runtime', {
+						useESModules: true
+					}]
+				]
+			}),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+			!dev && terser({
+				module: true
+			})
+		],
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
+		onwarn: function (warning) {
+            // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
+            // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+            if (warning.code === 'THIS_IS_UNDEFINED') {
+                return;
+            }
+            console.error(warning.message);
+        },
+	},
+
+	server: {
+		input: config.server.input(),
+		output: config.server.output(),
+		plugins: [
+			replace({
+				'process.browser': false,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+				generate: 'ssr',
+				dev
+			}),
+			resolve({
+				dedupe
+//                browser: true
+			}),
+			commonjs(),
+            json({
+                    namedExports: false,
+                    compact:!dev
+            })
+		],
+		external: Object.keys(pkg.dependencies).concat(
+			require('module').builtinModules || Object.keys(process.binding('natives'))
+		),
+
+		onwarn: function (warning) {
+            // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
+            // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+            if (warning.code === 'THIS_IS_UNDEFINED') {
+                return;
+            }
+            console.error(warning.message);
+        },
+	},
+
+	serviceworker: {
+		input: config.serviceworker.input(),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			commonjs(),
+			!dev && terser()
+		],
+
+		onwarn,
 	}
 };
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
-}
