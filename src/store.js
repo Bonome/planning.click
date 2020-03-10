08ea19ee0b373ba121415d4bf9d483e7acff8b45
Bonome <bonome@tak.lu>
2020-03-10 19:26:54 +0100

    stop scroll top when the start of the planning is reached

--- src/store.js
+++ src/store.js
@@ -3,4 +3,6 @@ import { writable } from 'svelte/store';
 export const selected_event = writable(0);
 
 export const number_week_displayed = writable(0);
+
+export const planning_start_date = writable(0);
 //export const slug = 
