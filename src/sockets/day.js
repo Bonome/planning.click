f68276d5e7b9c7e89930465226fb3044f637d30c
Bonome <bonome@tak.lu>
2020-03-12 19:57:38 +0100

    log each day actions

--- src/sockets/day.js
+++ src/sockets/day.js
@@ -1,5 +1,6 @@
 import * as engine from '../db/engine.js';
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID
 
 export const day = (socket, event) => {
     (async function () {
@@ -27,9 +28,22 @@ export const day = (socket, event) => {
             );
 //            assert.equal(1, r.upsertedCount+r.modifiedCount); 
 
+            let new_insert = false;
             if(r.upsertedId != null) {
                 event.data._id = r.upsertedId._id;
+                new_insert = true;
             }
+            db.collection('log_userday').insertOne(
+                {
+                    slug: event.data.slug,
+                    date: new Date(event.data.date),
+                    type: event.data.type,
+                    user: event.data.user,
+                    day_id: new_insert?event.data._id:new ObjectId(event.data._id),
+                    new: new_insert,
+                    created_at: new Date()
+                }
+            );
 
             socket.to(event.slug).emit('day', event.data);//emit to all others
             socket.emit('day', event.data);//emit back to emitter
